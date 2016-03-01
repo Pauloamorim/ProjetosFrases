10b4b85d02b051dbf0c6da0bc884e901435c2bca
Paulo <paulovitor.10@hotmail.com>
2016-02-29 21:14:25 -0300

    Commit inicial do projeto

---
+++ ProjetoFrases/src/main/java/com/ProjetoFrases/ProjetoFrasesApplication.java
@@ -0,0 +1,12 @@
+package com.ProjetoFrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFrasesApplication.class, args);
+	}
+}
