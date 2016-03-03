7c0b93d14cd07223709229230e17c8804afc5347
Paulo <paulovitor.10@hotmail.com>
2016-03-02 22:06:58 -0300

    Protótipo tabela consulta, consulta implementada usando jpaRepository, validações de campos obrigatórios,mensagem de erros

--- ProjetoFrases/src/main/resources/static/js/funcoes.js
+++ ProjetoFrases/src/main/resources/static/js/funcoes.js
@@ -1,4 +1,11 @@
 function limparCampos(){
+	
+	var divMensagem = document.getElementById('divMensagem');
+	if(divMensagem != null){
+		divMensagem.style.display='none';
+	}
+	
+	document.getElementById('tabela').style.display='none';
 	var elementos = document.getElementsByTagName('input');
 	for(var i = 0; i < elementos.length; i++){
 		elementos[i].value = '';
