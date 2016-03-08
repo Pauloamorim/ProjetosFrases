f9b67bd68e6e341d6fa466b23689511822c3dc3d
Paulo <paulovitor.10@hotmail.com>
2016-03-08 19:33:29 -0300

    adicionando protótipo menu

--- ProjetoFrases/src/main/java/com/ProjetoFrases/ProjetoFrasesApplication.java
+++ ProjetoFrases/src/main/java/com/ProjetoFrases/ProjetoFrasesApplication.java
@@ -2,11 +2,22 @@ package com.ProjetoFrases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProjetoFrasesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFrasesApplication.class, args);
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/index");
+		}
 	}
 }
